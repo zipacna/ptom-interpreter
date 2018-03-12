5ea5a76cc95e97bddeef73be1dc44ec6ac4550de
Jean <jeanmattes@gmx.de>
2018-03-12 13:48:11 +0100

    patch

---
+++ example.py
@@ -0,0 +1,4 @@
+from ptom import Ptom
+
+example = Ptom('test.ptom', 'load')
+# README.md example.__doc__
---
+++ ptom.py
@@ -0,0 +1,72 @@
+class Ptom:
+    """
+    plaintext object markdown - a minimalistic textformat
+    author: Jean Mattes
+    author-uri: http://www.risingcode.net/
+    """
+    tabsep = '%5Ct'
+    spacesep = '%20%20%20%20'
+    newlinesep = '%5Cn'
+
+    def __init__(self, file: str, op: str, txt='', raw=0):
+        """
+        :param file: to open
+        :param op: operation to be executed
+        """
+        self.file = file
+        self.handle = 0
+        self.ptom = 0
+        self.txt = txt
+        self.raw = raw
+        self.nodenew = ''
+        if op == 'load':
+            self.load()
+        elif op == 'dump':
+            self.dump()
+
+    def __str__(self):
+        """:return: Infos about current object"""
+        return '\nfile:{},\nhandle:{},\nptom:{},\ntxt:{},\nraw:{}'.format(self.file, self.handle,
+                                                                          self.ptom, self.txt,
+                                                                          self.raw, self.nodenew)
+
+    def load(self):
+        """
+        read the ptom file
+        # TODO: handle \t (or 4 spaces) & \n
+        """
+        self.handle = open(self.file, 'r')
+        self.ptom = self.handle.readlines()
+        for node in self.ptom:
+            for sep in ['\t', '    ']:
+                if sep in node:
+                    depth = node
+                    while isinstance(depth, list):
+                        # TODO: scan full range, replace 0 with all possibilities
+                        depth = depth[0]
+                    nodeop = node.replace(sep, '')
+                    self.nodenew = [nodeop]
+                else:
+                    self.nodenew = '-'
+        print(self.nodenew)
+        if self.raw == 0:
+            for line in self.ptom:
+                for sep in [self.tabsep, self.spacesep, self.newlinesep]:
+                    for sepn in ['\t', '\t', '\n']:
+                        line.replace(sep, sepn)
+        if isinstance(self.ptom, list):
+            print('\nLoading successful:', self.ptom)
+        else:
+            print('\nLoading failed')
+
+    def dump(self):
+        """
+        dump into ptom file
+        # TODO: handle \t (or 4 spaces) & \n
+        """
+        self.handle = open(self.file, 'w+')
+        self.ptom = self.handle.writelines(self.txt)
+        if self.ptom:
+            print('\nWriting successful')
+        else:
+            print('\nWriting failed')
